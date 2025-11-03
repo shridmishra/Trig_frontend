c3965da130ea78abef8ce4fe6d971b79fcd8e8b9
shridmishra <shridmishra00@gmail.com>
2025-11-03 16:50:57 +0530

    fix: continuous animation , removed scroll

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { GoogleGeminiEffect } from "../components/ui/google-gemini-effect";
+
+const page = () => {
+  return (
+    <div
+      className="h-screen bg-black w-full dark:border dark:border-white/10  relative overflow-clip"
+    >
+      <GoogleGeminiEffect />
+    </div>
+  );
+}
+
+export default page
